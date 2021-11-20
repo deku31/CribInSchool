8954f151b56a0892855c1f445f3c9cc312572a48
diotsani <dio.tsani123@gmail.com>
2021-11-21 00:02:55 +0700

    Update ngerapiin

--- kelas deri/Assets/script/WordSuffle/QuizDataScriptable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "QuestionsData", menuName = "QuestionsData", order = 1)]
-public class QuizDataScriptable : ScriptableObject
-{
-    public List<QuestionData> questions;
-}
