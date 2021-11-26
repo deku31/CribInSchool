e67712da1959eebcd7b903b65f74250aaece9f65
Deri Kurniawan <65839958+deku31@users.noreply.github.com>
2021-11-26 20:16:07 +0700

    update folder tanpa meta data

---
+++ pre alpha v0.0.7/Assets/Scripts/Gamemanager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Gamemanager : MonoBehaviour
+{
+    public player _PlayerManager;
+    public guruMangaer _GuruManger;
+    public PuzzleManager _PuzzleManager;
+    public JarakPandang _JarakPandang;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_PlayerManager.munculPuzzle==false)
+            {
+                Application.Quit();
+            }
+            else if (_PlayerManager.puzzle==true)
+            {
+                _PuzzleManager.keluar();
+            }
+        }
+    }
+}
