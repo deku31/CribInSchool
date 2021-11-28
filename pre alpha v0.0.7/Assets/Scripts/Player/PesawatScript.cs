fbff1f6b4b77961a924905ba6950c23ce4567aa1
diotsani <dio.tsani123@gmail.com>
2021-11-29 02:06:39 +0700

    update tes pesawat

---
+++ pre alpha v0.0.7/Assets/Scripts/Player/PesawatScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PesawatScript : MonoBehaviour
+{
+    public ProgressBarPlayer progresPlayer;
+    public int _progresPlayer;
+
+    private void Awake()
+    {
+        progresPlayer = GameObject.Find("gameplaymanager").GetComponent<ProgressBarPlayer>();
+    }
+
+    void Start()
+    {
+        _progresPlayer = progresPlayer.current;
+    }
+
+    public void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Score : " + _progresPlayer);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "TargetPesawat")
+        {
+            _progresPlayer++;
+            progresPlayer.current = _progresPlayer;
+            //Destroy(gameObject);
+            Debug.Log("Sudah Transfer");
+        }
+    }
+}
