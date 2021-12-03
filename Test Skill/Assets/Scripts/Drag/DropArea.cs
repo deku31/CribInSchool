ec1ca5d95cc29559f41abde79386839618b2b6d2
diotsani <dio.tsani123@gmail.com>
2021-12-04 00:37:35 +0700

    Project Test Skill

---
+++ Test Skill/Assets/Scripts/Drag/DropArea.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropArea : MonoBehaviour
+{
+    // Script ini memanggil dari script DropCondition
+    // Script ini digunakan sebagai area atau tujuan target skill slot
+    // Script ini akan otomatis dipanggil oleh script Equipment slot
+
+	public List<DropCondition> DropConditions = new List<DropCondition>();
+	public event Action<DraggableComponent> OnDropHandler;
+
+	public bool Accepts(DraggableComponent draggable)
+	{
+		return DropConditions.TrueForAll(cond => cond.Check(draggable));
+	}
+
+	public void Drop(DraggableComponent draggable)
+	{
+		OnDropHandler?.Invoke(draggable);
+	}
+}
---
+++ Test Skill/Assets/Scripts/Drag/EquipmentSlot.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class EquipmentSlot : MonoBehaviour
+{
+    // Script ini digunakan sebagai mengatur posisi dari target skill slot biar bisa tepat ditengah
+    // Script ini akan otomatis memanggil script DropArea
+    // Script yang aku komenkan itu script yang tidak digunakan atau hasil copas yang tidak kuhapus wkwkw
+
+	protected DropArea DropArea;
+	//protected DraggableComponent CurrentItem = null;
+
+	//private DisableDropCondition disableDropCondition;
+
+	protected virtual void Awake()
+	{
+		DropArea = GetComponent<DropArea>() ?? gameObject.AddComponent<DropArea>();
+		DropArea.OnDropHandler += OnItemDropped;
+		//disableDropCondition = new DisableDropCondition();
+	}
+
+	private void OnItemDropped(DraggableComponent draggable)
+	{
+        draggable.upgradeButton.SetActive(false);
+
+		draggable.transform.position = transform.position;
+		//CurrentItem = draggable;
+		//DropArea.DropConditions.Add(disableDropCondition);
+		//draggable.OnBeginDragHandler += CurrentItemOnBeginDrag;
+	}
+
+    //public void Initialize(DraggableComponent currentItem)
+    //{
+        //if (currentItem == null)
+        //{
+           //Debug.LogError("Tried to initialize the slot with an null item!");
+            //return;
+        //}
+
+        //OnItemDropped(currentItem);
+    //}
+
+    //Current item is being dragged so we listen for the EndDrag event
+    //private void CurrentItemOnBeginDrag(PointerEventData eventData)
+    //{
+    //CurrentItem.OnEndDragHandler += CurrentItemEndDragHandler;
+    //}
+
+    //private void CurrentItemEndDragHandler(PointerEventData eventData, bool dropped)
+    //{
+    //CurrentItem.OnEndDragHandler -= CurrentItemEndDragHandler;
+
+    //if (!dropped)
+    //{
+    //return;
+    //}
+
+    //DropArea.DropConditions.Remove(disableDropCondition); //We dropped the component in another slot so we can remove the DisableDropCondition
+    //CurrentItem.OnBeginDragHandler -= CurrentItemOnBeginDrag; //We make sure to remove this listener as the item is no longer in this slot
+    //CurrentItem = null; //We no longer have an item in this slot, so we remove the refference
+    //}
+}
