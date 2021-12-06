5ba306efbce8bb933cc46f12b828564f3f05a2a4
diotsani <dio.tsani123@gmail.com>
2021-12-07 01:48:43 +0700

    update

---
+++ Test Skill/Assets/Scripts/Drag/LoadSkill.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSkill : MonoBehaviour
+{
+    public GameObject[] skillPrefabs;
+    public Transform spawnPointSkill_1;
+    public Transform spawnPointSkill_2;
+
+    public GameObject prefab_1;
+    public GameObject prefab_2;
+
+    public GameObject clone_1;
+    public GameObject clone_2;
+
+    public void Awake()
+    {
+        //clone_1 = GameObject.FindGameObjectWithTag("Skill");
+        //dragScript = GameObject.FindGameObjectWithTag("Skill");
+    }
+
+    public void Start()
+    {
+        
+
+        int selectSkill_1 = PlayerPrefs.GetInt("selectSkill_1");
+        int selectSkill_2 = PlayerPrefs.GetInt("selectSkill_2");
+
+        prefab_1 = skillPrefabs[selectSkill_1];
+        prefab_2 = skillPrefabs[selectSkill_2];
+
+        //GameObject clone_1 = Instantiate(prefab_1, spawnPointSkill_1.position, Quaternion.identity);
+        //GameObject clone_2 = Instantiate(prefab_2, spawnPointSkill_2.position, Quaternion.identity);
+
+        clone_1 = Instantiate(prefab_1, spawnPointSkill_1.position, prefab_1.transform.rotation);
+        clone_2 = Instantiate(prefab_2, spawnPointSkill_2.position, prefab_2.transform.rotation);
+    }
+}
---
+++ Test Skill/Assets/Scripts/Drag/SkillSetting.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillSetting : MonoBehaviour
+{
+    public GameObject dragScript_1;
+    public GameObject dragScript_2;
+    public GameObject dragScript_3;
+    public GameObject dragScript_4;
+    public GameObject dragScript_5;
+
+    public LoadSkill loadSkill;
+
+    public void Update()
+    {
+        dragScript_1 = FindInActiveObjectByName("Skill1");
+        dragScript_2 = FindInActiveObjectByName("Skill2");
+        dragScript_3 = FindInActiveObjectByName("Skill3");
+        dragScript_4 = FindInActiveObjectByName("Skill4");
+        dragScript_5 = FindInActiveObjectByName("Skill5");
+
+
+        dragScript_1.GetComponent<DraggableComponent>().enabled = false;
+        dragScript_2.GetComponent<DraggableComponent>().enabled = false;
+        dragScript_3.GetComponent<DraggableComponent>().enabled = false;
+        dragScript_4.GetComponent<DraggableComponent>().enabled = false;
+        dragScript_5.GetComponent<DraggableComponent>().enabled = false;
+
+        //dragScript_1.transform.position = loadSkill.spawnPointSkill_1;
+    }
+
+    
+
+    GameObject FindInActiveObjectByName(string name) //fungsi mencari object yang tidak aktif menggunakan nama
+    {
+        Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
+        for (int i = 0; i < objs.Length; i++)
+        {
+            if (objs[i].hideFlags == HideFlags.None)
+            {
+                if (objs[i].name == name)
+                {
+                    return objs[i].gameObject;
+                }
+            }
+        }
+        return null;
+    }
+
+    GameObject FindInActiveObjectByTag(string tag) //fungsi mencari object yang tidak aktif menggunakan tag
+    {
+
+        Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
+        for (int i = 0; i < objs.Length; i++)
+        {
+            if (objs[i].hideFlags == HideFlags.None)
+            {
+                if (objs[i].CompareTag(tag))
+                {
+
+                    return objs[i].gameObject;
+                }
+            }
+        }
+        return null;
+    }
+}
