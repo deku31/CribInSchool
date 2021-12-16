2be6ebd912c93c9f4ee4f798b0168886a11bfe36
diotsani <dio.tsani123@gmail.com>
2021-12-17 03:40:43 +0700

    update

---
+++ pre alpha v0.0.8/Assets/Scripts/Skill Aktif/AktifSkill_2.cs
@@ -0,0 +1,163 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+using System;
+
+public class AktifSkill_2 : MonoBehaviour
+{
+    public static Action<Vector3> OnDistraction;
+
+    public NavMeshAgent agentGuru;
+    public Gamemanager gm;
+
+    [SerializeField]
+    private GameObject batu;
+
+    public bool skillAktif;
+    public bool distractSkill;
+
+    private bool _distracted;
+
+    private void Awake()
+    {
+        //agentGuru = GameObject.FindGameObjectWithTag("guru").GetComponent<NavMeshAgent>();
+
+        gm = FindObjectOfType<Gamemanager>();
+        if (gm != null)
+        {
+            skillAktif = true;
+        }
+
+        OnDistraction += ThrowCoin;
+    }
+
+    void Start()
+    {
+        OnDistraction += ThrowCoin;
+        AktifSkill_2.OnDistraction += GetDistracted;
+
+        //agentGuru = GameObject.Find("guru").GetComponent<NavMeshAgent>();
+        //agentGuru = FindInActiveObjectByTag("guru").GetComponent<NavMeshAgent>();
+    }
+
+    public void Update()
+    {
+        //agentGuru = FindInActiveObjectByName("Guru(Clone)").GetComponent<NavMeshAgent>();
+
+        if (distractSkill == true)
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out RaycastHit hitInfo))
+                {
+                    if (!(OnDistraction is null))
+                    {
+                        OnDistraction(hitInfo.point);
+                    }
+                }
+            }
+        }
+    }
+
+    public void Distract()
+    {
+        if (gm != null)
+        {
+            if (skillAktif == true)
+            {
+                distractSkill = true;
+                skillAktif = false;
+            }
+            else
+            {
+                print("proses");
+            }
+        }
+        else
+        {
+            print("balm ada gm");
+        }
+    }
+
+    void activeSkills()
+    {
+        distractSkill = false;
+    }
+
+    void activeButtonSkills()
+    {
+        skillAktif = true;
+    }
+
+    private void ThrowCoin(Vector3 pos)
+    {
+        Instantiate(batu, pos, Quaternion.identity);
+    }
+
+    private void OnDestroy()
+    {
+        AktifSkill_2.OnDistraction -= GetDistracted;
+        //OnDistraction -= ThrowCoin;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_distracted)
+            return;
+    }
+
+    private void GetDistracted(Vector3 pos)
+    {
+        if (Vector3.Distance(transform.position, pos)<= 15f)
+        {
+            StopAllCoroutines();
+            _distracted = true;
+            agentGuru.SetDestination(pos);
+            StartCoroutine(FollowDistraction(pos));
+        }
+    }
+
+    private IEnumerator FollowDistraction(Vector3 pos)
+    {
+        while (Vector3.Distance(transform.position, pos) > 2f)
+            yield return null;
+        agentGuru.SetDestination(transform.position);
+    }
+
+    GameObject FindInActiveObjectByTag(string tag) //fungsi mencari object yang tidak aktif menggunakan tag
+    {
+
+        Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
+        for (int i = 0; i < objs.Length; i++)
+        {
+            if (objs[i].hideFlags == HideFlags.None)
+            {
+                if (objs[i].CompareTag(tag))
+                {
+
+                    return objs[i].gameObject;
+                }
+            }
+        }
+        return null;
+    }
+
+    GameObject FindInActiveObjectByName(string name) //fungsi mencari object yang tidak aktif menggunakan nama
+    {
+        Transform[] objs = Resources.FindObjectsOfTypeAll<Transform>() as Transform[];
+        for (int i = 0; i < objs.Length; i++)
+        {
+            if (objs[i].hideFlags == HideFlags.None)
+            {
+                if (objs[i].name == name)
+                {
+                    return objs[i].gameObject;
+                }
+            }
+        }
+        return null;
+    }
+}
